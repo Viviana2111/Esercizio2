8bdcd7973a8bcdda617dd14258e2b8f0daf0376e
fosch <fosch@LAPTOP-SPTCPTQI>
2023-03-07 16:02:13 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Esercizio2
+{
+    internal class Persona
+
+    {
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+        // di seguiot ho inserito tutti campi 0 perchè facenti parti di metodi diversi
+        public void getnome()
+        {
+            Console.WriteLine("nome {0}", Nome);
+        }
+        public void getcognome()
+        {
+            Console.WriteLine("cognome {0}", Cognome);
+        }
+        public void geteta()
+        {
+            Console.WriteLine("età {0}", Eta);
+        }
+        // qui ho inserito 0,1,2, perchè tutti appartententi allo stesso metodo 
+        public void Dettagli()
+
+        {
+            Console.WriteLine("Questi sono i dettagli della persona {0} {1} {2}", Nome, Cognome, Eta);
+        }
+        static void Main(string[] args)
+        {
+            Persona andrea = new Persona();
+            andrea.Nome = "Andrea";
+            andrea.Cognome = "Andrei";
+            andrea.Eta = 22;
+
+            Persona lorenzo = new Persona();
+            lorenzo.Nome = "Lorenzo";
+            lorenzo.Cognome = "Scritto";
+            lorenzo.Eta = 23;
+
+
+        }
+    }
+}
